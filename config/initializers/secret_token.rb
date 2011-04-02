94e6bf2b39fe6fc0ff622a03ba47d898b85a0f1a
Valdilanio <dot_v@hotmail.com>
2011-04-02 18:31:48 -0300

    Estrutura projeto site-devinsantos com rails 3.0.5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteDevinsantos::Application.config.secret_token = 'c1f7ec3ed44a422b4baca53da388c53ed3ed3041d2e7b0505328ceed771fb339e77585ffaf7a3b0c99a08323fbc733250b321d2b33842437b50431eb69c69b90'
